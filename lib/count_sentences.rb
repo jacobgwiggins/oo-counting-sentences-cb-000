a7dda54ce9318ac29c110d251fa771090eb3d703
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-09-23 21:52:15 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,21 +1,21 @@
 # require 'pry'
-# 
+#
 # class String
-# 
+#
 #   def sentence?
-#     
+#
 #   end
-# 
+#
 #   def question?
-# 
+#
 #   end
-# 
+#
 #   def exclamation?
-# 
+#
 #   end
-# 
+#
 #   def count_sentences
-# 
+#
 #   end
 # end
 
